44645244eb78bd90f3aeb456d6d323abc1e134ef
jonnyboi <54411950+j0nnyboi@users.noreply.github.com>
2022-04-22 18:03:15 +0100

    Update ids.ts

--- js/packages/web/src/utils/ids.ts
+++ js/packages/web/src/utils/ids.ts
@@ -2,5 +2,5 @@ import { PublicKey } from '@safecoin/web3.js';
 
 // TODO: generate key ---
 export const AR_SOL_HOLDER_ID = new PublicKey(
-  '6FKvsq4ydWFci6nGq9ckbjYMtnmaqAoatz5c9XWjiDuS',
+  'es7DKe3NyR1u8MJNuv6QV6rbhmZQkyYUpgKpGJNuTTc ',
 );
