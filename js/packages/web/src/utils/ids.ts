f4b32815b98e32de398ba5f545609ee0e3c10ec3
Araviel <contact@araviel.io>
2022-04-27 17:45:30 +0200

    auto generated & fix AR id

--- js/packages/web/src/utils/ids.ts
+++ js/packages/web/src/utils/ids.ts
@@ -2,5 +2,5 @@ import { PublicKey } from '@safecoin/web3.js';
 
 // TODO: generate key ---
 export const AR_SOL_HOLDER_ID = new PublicKey(
-  'es7DKe3NyR1u8MJNuv6QV6rbhmZQkyYUpgKpGJNuTTc ',
+  'es7DKe3NyR1u8MJNuv6QV6rbhmZQkyYUpgKpGJNuTTc',
 );
