e0f056bf7372a791a5ef7a62691737db81323a87
jonnyboi <54411950+j0nnyboi@users.noreply.github.com>
2022-05-01 15:13:21 +0000

    Updates

---
+++ _next/static/V0qkvZoKB2TGQd5m39xAh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-39692860ab8ac09c13a1.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
